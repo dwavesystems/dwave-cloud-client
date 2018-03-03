c6a13916e3a643b57bbe8be07bfb2cc6d86eeaa1
Radomir Stevanovic <radomir@dwavesys.com>
2018-03-02 19:58:25 -0800

    Add emulated QPU interface (dwave.cloud.sw.Client)

---
+++ dwave/cloud/sw.py
@@ -0,0 +1,20 @@
+"""
+Interface to software samplers API.
+
+Software samplers emulate the QPU behavior.
+"""
+from __future__ import absolute_import
+
+from dwave.cloud.qpu import Client as _QPUClient
+
+
+class Client(_QPUClient):
+
+    @staticmethod
+    def is_solver_handled(solver):
+        """Predicate function that determines if the given solver should be
+        handled by this client.
+        """
+        if not solver:
+            return False
+        return solver.id.startswith('c4-sw_')
