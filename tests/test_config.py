bb08728fe8dba658e4c408c727d529e43ca574e7
Radomir Stevanovic <radomir@dwavesys.com>
2018-03-06 19:05:53 -0800

    Add tests for generalized config loading

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,7 +3,9 @@ from __future__ import absolute_import, print_function
 import os
 import sys
 import unittest
-import textwrap
+import configparser
+
+from functools import partial
 
 try:
     # python 3
@@ -29,7 +31,8 @@ except ImportError:
     configparser_open_namespace = "backports.configparser.open"
 
 
-from dwave.cloud.config import detect_configfile_path, load_config_from_file, load_profile
+from dwave.cloud.config import (
+    detect_configfile_path, load_config_from_file, load_profile, load_config)
 
 
 class TestConfig(unittest.TestCase):
@@ -54,7 +57,12 @@ class TestConfig(unittest.TestCase):
         token = alpha-token
     """
 
-    def test_config_load(self):
+    def parse_config_string(self, text):
+        config = configparser.ConfigParser(default_section="defaults")
+        config.read_string(text)
+        return dict(config)
+
+    def test_config_load_from_file(self):
         with mock.patch(configparser_open_namespace, iterable_mock_open(self.config_body), create=True):
             config = load_config_from_file(filename="filename")
             self.assertEqual(config.sections(), ['dw2000', 'software', 'alpha'])
@@ -106,3 +114,53 @@ class TestConfig(unittest.TestCase):
     def test_config_file_detection_nonexisting(self):
         with mock.patch("os.path.exists", lambda path: False):
             self.assertEqual(detect_configfile_path(), None)
+
+
+    def _assert_config_valid(self, config):
+        # profile is loaded
+        self.assertEqual(config['endpoint'], "https://url.to.alpha/api")
+        # default values are inherited
+        self.assertEqual(config['client'], "qpu")
+
+    def _load_config_from_file(self, asked, provided):
+        self.assertEqual(asked, provided)
+        return self.parse_config_string(self.config_body)
+
+
+    def test_config_load_configfile_arg(self):
+        with mock.patch("dwave.cloud.config.load_config_from_file",
+                        partial(self._load_config_from_file, provided='myfile')):
+            self._assert_config_valid(load_config(config_file='myfile', profile='alpha'))
+
+    def test_config_load_configfile_env(self):
+        with mock.patch("dwave.cloud.config.load_config_from_file",
+                        partial(self._load_config_from_file, provided='myfile')):
+            os.environ = {'DWAVE_CONFIG_FILE': 'myfile'}
+            self._assert_config_valid(load_config(config_file=None, profile='alpha'))
+
+    def test_config_load_configfile_detect(self):
+        with mock.patch("dwave.cloud.config.load_config_from_file",
+                        partial(self._load_config_from_file, provided=None)):
+            self._assert_config_valid(load_config(config_file=None, profile='alpha'))
+
+    def test_config_load_configfile_detect_profile_env(self):
+        with mock.patch("dwave.cloud.config.load_config_from_file",
+                        partial(self._load_config_from_file, provided=None)):
+            os.environ = {'DWAVE_PROFILE': 'alpha'}
+            self._assert_config_valid(load_config())
+
+    def test_config_load_configfile_env_profile_env(self):
+        with mock.patch("dwave.cloud.config.load_config_from_file",
+                        partial(self._load_config_from_file, provided='myfile')):
+            os.environ = {'DWAVE_CONFIG_FILE': 'myfile', 'DWAVE_PROFILE': 'alpha'}
+            self._assert_config_valid(load_config())
+
+    def test_config_load_configfile_env_profile_env_key_arg(self):
+        with mock.patch("dwave.cloud.config.load_config_from_file",
+                        partial(self._load_config_from_file, provided='myfile')):
+            os.environ = {'DWAVE_CONFIG_FILE': 'myfile', 'DWAVE_PROFILE': 'alpha'}
+            self.assertEqual(load_config(endpoint='manual')['endpoint'], 'manual')
+            self.assertEqual(load_config(token='manual')['token'], 'manual')
+            self.assertEqual(load_config(client='manual')['client'], 'manual')
+            self.assertEqual(load_config(solver='manual')['solver'], 'manual')
+            self.assertEqual(load_config(proxy='manual')['proxy'], 'manual')
