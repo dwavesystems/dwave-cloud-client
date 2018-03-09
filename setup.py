a47336cc0108189076bbc5eb5377813d51b8c29b
Radomir Stevanovic <radomir@dwavesys.com>
2018-03-08 17:32:32 -0800

    Bump to version 0.3.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ packages = [package for package in find_packages() if package.startswith('dwave'
 
 setup(
     name='dwave-cloud-client',
-    version='0.3.0',
+    version='0.3.1',
     author='D-Wave Systems Inc.',
     description='A minimal client for interacting with D-Wave cloud resources',
     url='https://github.com/dwavesystems/dwave-cloud-client',
