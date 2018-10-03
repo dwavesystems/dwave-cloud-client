8c6d8d57aa79248543eb0a4482407fd53b4f9b31
Radomir Stevanovic <radomir@dwavesys.com>
2018-10-03 15:33:18 -0700

    Add Apache 2.0 license headers

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,8 +1,23 @@
+# Copyright 2017 D-Wave Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Tests the ability to connect to the SAPI server with the `dwave.cloud.qpu.Client`.
 
 test_mock_solver_loading.py duplicates some of these tests against a mock server.
 """
+
 from __future__ import absolute_import
 
 import unittest
