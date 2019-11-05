ee3299ca7939fb85e027eaa0d04220be623ce3ad
Radomir Stevanovic <radomir@dwavesys.com>
2019-11-05 09:11:26 -0800

    Release version 0.6.2
    Changes since 0.6.1:
    
    New Features
    ---
    
    - Multipart problem upload, resilient to network failures,
      with cold restart (#326)
    - By-ref problem submit to unstructured solvers (#327)
    - CLI command for problem upload: `dwave upload`
    - Accept custom external HTTP headers in config/env var/
      `Client` constructor/factory (#334)
    - Better external log level control (#336)
    
    Fixes
    ---
    
    - Thread-safe `requests.Session`s (#182)
    - Solver graph respected in `dwave ping` on structured solvers (#332)
    - Correct error handling in `dwave ping` on unstructured solvers (#329)

--- dwave/cloud/package_info.py
+++ dwave/cloud/package_info.py
@@ -14,7 +14,7 @@
 
 __packagename__ = 'dwave-cloud-client'
 __title__ = 'D-Wave Cloud Client'
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 __author__ = 'D-Wave Systems Inc.'
 __description__ = 'A minimal client for interacting with D-Wave cloud resources.'
 __url__ = 'https://github.com/dwavesystems/dwave-cloud-client'
