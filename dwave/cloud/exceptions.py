7e8ca5cf3e5ae3eb74d1fcdb4bb7841d2b81858a
Radomir Stevanovic <radomir@dwavesys.com>
2021-04-30 15:21:08 -0700

    Fix py36 import error

--- dwave/cloud/exceptions.py
+++ dwave/cloud/exceptions.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import dwave.cloud.api.exceptions as api
+from dwave.cloud.api import exceptions as api
 
 
 # alias for backward compatibility
